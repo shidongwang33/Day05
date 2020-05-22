d8d63af68001d3c68e780b6a4dc18fdf4dd40638
sdw <shidongwang@QQ.com>
2020-05-22 15:13:03 +0800

    第一次提交

---
+++ src/main/java/com/shidongwang/bean/Hello.java
@@ -0,0 +1,8 @@
+package com.shidongwang.bean;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		System.out.println("helloWord");
+	}
+}
---
+++ src/test/java/com/shidongwang/bean/Hello2.java
@@ -0,0 +1,8 @@
+package com.shidongwang.bean;
+
+public class Hello2 {
+
+	public static void main(String[] args) {
+		System.out.println("helloWord2");
+	}
+}
